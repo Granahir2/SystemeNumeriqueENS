c1c5f811fb691d9a087158f22c628a5e1d4e0ac8
Tu <tubmt01@gmail.com>
2024-01-13 12:37:04 +0100

    extend to full register file

--- register_file/utils.py
+++ register_file/utils.py
@@ -28,7 +28,7 @@ def dup32(b):
     return b
 
 def mux2to1(d0, d1, s):
-    return (And(d0,(~dup32(s))))|(And(d1,dup32(s)))
+    return (And(d0, (~dup32(s))))|(And(d1, dup32(s)))
 
 def mux4to1(d, s):
     return mux2to1(mux2to1(d[0], d[1], Select(0, s)), mux2to1(d[2], d[3], Select(0, s)), Select(1, s))
@@ -39,14 +39,14 @@ def mux8to1(d, s):
 def mux32to1(d, s):
     return mux4to1([mux8to1(d[i:i+8], Slice(0, 3, s)) for i in range(0, 32, 8)], Slice(3, 5, s))
 
-# def b32mux2to1(d0, d1, s):
-#     return [mux2to1(d0[i], d1[i], s) for i in range(32)]
-
-# def b32mux32to1(d, s):
-#     return [mux32to1([d[i][j] for i in range(32)], s) for j in range(32)]
-
-# def union(l):
-#     res = l[0]
-#     for i in range(1, len(l)):
-#         res = res + l[i]
-#     return res
+def cmp(a, b):
+    # return two bits for a < b, a = b
+    lt, eq = Constant("0"), Constant("1")
+    for i in range(31, -1, -1):
+        ai = Select(i, a)
+        bi = Select(i, b)
+        lt = lt | (eq & (~ai) & bi)
+        eq = eq & (~(ai ^ bi))
+    lt.set_as_output("lt")
+    eq.set_as_output("eq1")
+    return (lt, eq)
